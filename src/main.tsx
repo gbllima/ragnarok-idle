75cc982ca212172e9573f8652d7eb60bb1180888
Gabriel Lima <51611613+gbllima@users.noreply.github.com>
2026-09-13 12:06:49 -0300

    Enable generated Swordsman animations

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import { installMonsterSpriteWatcher } from './game/monsterSprites'
+import { installPlayerSpriteWatcher } from './game/playerSprites'
 import { installWorldMovement } from './game/worldMovement'
 import './styles.css'
 import './death.css'
 import './progression.css'
 import './world.css'
+import './playerSprites.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -15,4 +17,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 )
 
 installMonsterSpriteWatcher()
+installPlayerSpriteWatcher()
 installWorldMovement()
